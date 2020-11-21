3109509eb4f129adbf6865022df1495cc4c23ac2
coder787 <43294866+coder787@users.noreply.github.com>
2020-11-21 17:09:21 +0000

    fixed server.js

--- twotter/server.js
+++ twotter/server.js
@@ -5,7 +5,7 @@ const enforce = require('express-sslify');
 
 const app = express();
 
-app.use(enforce.HTTPS({ _trustProtoHeader: true }));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(serveStatic(__dirname + '/dist'));
 app.use(history());
 
